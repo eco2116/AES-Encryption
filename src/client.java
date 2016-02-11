f36e0f5add33c23c2e32e0003f4ddeedcf71642e
evanoconnor <eco2116@columbia.edu>
2016-02-10 20:00:10 -0500

    More server client work, input validation

--- src/client.java
+++ src/client.java
@@ -1,23 +1,131 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+// TODO: cite this: http://www.rgagnon.com/javadetails/java-0542.html
 public class client {
 
-    public static void main(String argv[]) throws Exception {
+    public final static int SOCKET_PORT = 13267;      // you may change this
+    public final static String SERVER = "127.0.0.1";  // localhost
+    public final static String
+            FILE_TO_RECEIVED = "/Users/evanoconnor/dev/security/programming1/test.txt";  // you may change this, I give a
+    // different name because i don't want to
+    // overwrite the one used by server...
 
-        String msg;
-        String response;
+    public final static int FILE_SIZE = 6022386; // file size temporary hard coded
+    // should bigger than the file to be downloaded
 
-        BufferedReader clientIn = new BufferedReader(new InputStreamReader(System.in));
-        Socket clientSocket = new Socket("localhost", 6789);
-        DataOutputStream serverOut = new DataOutputStream(clientSocket.getOutputStream());
-        BufferedReader serverIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        msg = clientIn.readLine();
-        serverOut.writeBytes(msg);
-        response = serverIn.readLine();
-        System.out.println("I got from server: " + response);
-        clientSocket.close();
+    public static void main (String[] args) {
+
+
+        int bytesRead;
+        int current = 0;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket sock = null;
+        try {
+
+            try {
+                sock = new Socket(SERVER, SOCKET_PORT);
+            } catch(UnknownHostException e) {
+                failWithMessage("Could not create socket due to unknown host.");
+            } catch(IOException e) {
+                failWithMessage("Error creating socket. Unexpected IOException was thrown.");
+            }
+
+            System.out.println("Connecting...");
+
+            // receive file
+            byte [] mybytearray  = new byte [FILE_SIZE];
+
+            try {
+                InputStream is = sock.getInputStream();
+            } catch()
+
+
+
+            fos = new FileOutputStream(FILE_TO_RECEIVED);
+            bos = new BufferedOutputStream(fos);
+            bytesRead = is.read(mybytearray,0,mybytearray.length);
+            current = bytesRead;
+
+            do {
+                bytesRead =
+                        is.read(mybytearray, current, (mybytearray.length-current));
+                if(bytesRead >= 0) current += bytesRead;
+            } while(bytesRead > -1);
+
+            bos.write(mybytearray, 0 , current);
+            bos.flush();
+            System.out.println("File " + FILE_TO_RECEIVED
+                    + " downloaded (" + current + " bytes read)");
+        }
+        finally {
+            closeStreamsAndSocket(fos, bos, sock);
+        }
     }
+
+    private static String validatePassword(String input) {
+        if(input.length() != 16) {
+            failWithMessage("Password must be 16 characters long.");
+        } else if(!input.matches("[A-Za-z0-9]+")) {
+            failWithMessage("Password must only contain alphanumeric characters.");
+        }
+        return input;
+    }
+
+    // TODO: validate file name
+
+    private static String validateIP(String input) {
+        // TODO: implement
+        return input;
+    }
+
+    private static int validatePort(String input) {
+        int port = 0;
+        try {
+            port = Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            failWithMessage("Port must be an integer");
+        }
+        if(port > 65536) {
+            failWithMessage("Port value out of range. Must be <= 6535");
+        }
+        return port;
+    }
+
+    private static void failWithMessage(String msg) {
+        System.out.println("Client-side error encountered.");
+        System.out.println(msg);
+        System.exit(0);
+    }
+
+    private static void closeStreamsAndSocket(FileOutputStream fos, BufferedOutputStream bos, Socket sock) {
+        try {
+            if (fos != null) fos.close();
+            if (bos != null) bos.close();
+            if (sock != null) sock.close();
+        } catch(IOException e) {
+            failWithMessage("Unexpected IOException encountered while closing streams and sockets.");
+        }
+
+    }
+/**
+ * Password: The 16 character password may contain any alphanumeric character (i.e. lowercase,
+ uppercase and digits). Note: special characters are not included in order to simplify the input.
+ • filename: Clearly indicate in your README file if the path of the file provided as input must be the
+ full path or relevant to the directory containing the executable. You may just require that the file be in
+ the same directory as the executable.
+ • server IP address or name
+ • port number to use when contacting the server
+ • Necessary RSA key components: any inputs (filenames) to provide the client the necessary
+ information for the RSA keys . Key components should be read from files and not have to be typed
+ by the user.
+ */
 }
--- src/server.java
+++ src/server.java
@@ -1,30 +1,94 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
 
-    public static void main(String argv[]) throws Exception {
+    public final static int SOCKET_PORT = 13267;  // you may change this
+    public final static String FILE_TO_SEND = "/Users/evanoconnor/dev/security/programming1/test.txt";  // you may change this
 
-        String clientMsg;
+    public static void main (String[] args) throws IOException {
 
-        // TODO: Input port
-        ServerSocket serverSocket = new ServerSocket(6789);
+        // Input and validate client parameters
+        int port = validatePort(args[0]);
+        String trustedMode = validateTrustMode()
 
-        // TODO: handle break
-        while(true) {
-
-            Socket connection = serverSocket.accept();
-            BufferedReader clientIn = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            DataOutputStream clientOut = new DataOutputStream(connection.getOutputStream());
-
-            clientMsg = clientIn.readLine();
-            System.out.println("receieved: " + clientMsg);
-
-            clientOut.writeBytes("server receieved: " + clientMsg + "\n");
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        OutputStream os = null;
+        ServerSocket servsock = null;
+        Socket sock = null;
+        try {
+            servsock = new ServerSocket(SOCKET_PORT);
+            while (true) {
+                System.out.println("Waiting...");
+                try {
+                    sock = servsock.accept();
+                    System.out.println("Accepted connection : " + sock);
+                    // send file
+                    File myFile = new File (FILE_TO_SEND);
+                    byte [] mybytearray  = new byte [(int)myFile.length()];
+                    fis = new FileInputStream(myFile);
+                    bis = new BufferedInputStream(fis);
+                    bis.read(mybytearray,0,mybytearray.length);
+                    os = sock.getOutputStream();
+                    System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+                    os.write(mybytearray,0,mybytearray.length);
+                    os.flush();
+                    System.out.println("Done.");
+                }
+                finally {
+                    if (bis != null) bis.close();
+                    if (os != null) os.close();
+                    if (sock!=null) sock.close();
+                }
+            }
+        }
+        finally {
+            if (servsock != null) servsock.close();
         }
     }
+
+    private static int validatePort(String input) {
+        int port = 0;
+        try {
+            port = Integer.parseInt(input);
+        } catch(NumberFormatException e) {
+            failWithMessage("Port must be an integer");
+        }
+        if(port > 65536) {
+            failWithMessage("Port value out of range. Must be <= 6535");
+        }
+        return port;
+    }
+
+    private static String validateTrustMode(String input) {
+        if(!input.equals("t") && !input.equals("u")) {
+            failWithMessage("Trusted mode must be set to t or u");
+        }
+        return input;
+    }
+
+    private static void failWithMessage(String msg) {
+        System.out.println("Server-side error encountered.");
+        System.out.println(msg);
+        System.exit(0);
+    }
+
+    // TODO: Validate RSA file names
+
+    /**
+     *  The port number on which the server will listen for a connection from the client.
+     • mode: A single lowercase character of t or u. t means trusted mode, u means untrusted mode (file
+     gets replaced).
+     • Necessary RSA key components: any inputs (filenames) to provide the server the necessary
+     information for the RSA keys . Key components should be read from files and not have to be typed
+     by the user.
+     Notes on the details
+     */
 
 }
